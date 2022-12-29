97aafff0035bf24cf66eeedad0f950ec732e2bde
chondosha1234 <jmiller61193@gmail.com>
2022-12-29 14:05:21 -0500

    updated functional test

--- functional_test.py
+++ functional_test.py
@@ -16,16 +16,27 @@ class NewVisitorTest(unittest.TestCase):
 
         #page title mentions 'To Do' list
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         #user invited to enter to-do item
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         #user types "buy bread" into text box
+        inputbox.send_keys('Buy Bread')
         #when user presses enter the page updates and has list
         #"1: Buy Bread" as item on list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy Bread' for row in rows))
 
         #There is still text box entry for more items
-
+        self.fail('Finish Test')
         #user enters "buy milk"
         #page updates again and now has 2 entries
 
