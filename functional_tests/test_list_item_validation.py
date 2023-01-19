63f0bd39f0192154cfbd4e325941d0464b3cdb13
chondosha1234 <jmiller61193@gmail.com>
2023-01-19 08:32:03 -0500

    added functional test for hiding error messages

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,6 +6,9 @@ from django.urls import reverse
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element(By.CSS_SELECTOR, '.has-error')
+
     def test_cannot_add_empty_list_items(self):
         #User goes to homepage and tries to submit blank list item
         #they hit enter
@@ -55,6 +58,27 @@ class ItemValidationTest(FunctionalTest):
 
         # user sees error message
         self.wait_for(lambda:self.assertEqual(
-            self.browser.find_element(By.CSS_SELECTOR, '.has-error').text,
+            self.get_error_element().text,
             "You've already got this in your list"
+        ))
+
+    def test_error_messages_clear_on_input(self):
+        # user starts list and causes validation error
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Buy cake')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_table('1: Buy cake')
+        self.get_item_input_box().send_keys('Buy cake')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda:self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # user types in input box to clear error
+        self.get_item_input_box().send_keys('a')
+
+        # error message disappears
+        self.wait_for(lambda:self.assertFalse(
+            self.get_error_element().is_displayed()
         ))
