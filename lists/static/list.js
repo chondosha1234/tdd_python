e2e3f2649136f33b1efbf9823f46ada780759ee1
chondosha1234 <jmiller61193@gmail.com>
2023-01-19 11:26:15 -0500

    added namespace for initialize func and added script to base.html

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,7 @@
-
-var initialize = function() {
+// make object of which is property of global window variable, use as namespace
+window.superlists = {};
+// make initialize an attribute of namespace object
+window.superlists.initialize = function() {
   $('input[name="text"]').on('keypress', function(){
     $('.has-error').hide();
   });
