b62afb5ab45d6c72630b9fac52b266bbf8db3e39
chondosha1234 <jmiller61193@gmail.com>
2023-01-27 08:36:48 -0500

    removed empty line

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -2,7 +2,6 @@ from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-
 from django.urls import reverse
 import time
 
